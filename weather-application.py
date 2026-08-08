5489ebf475bb322fcc33a28e9c4ed10a78533522
JD-Jaidev <artificialintelligencejai@gmail.com>
2026-08-08 12:49:51 +0530

    feat : weather application project in python

--- weather-application.py
+++ weather-application.py
@@ -5,6 +5,7 @@ import requests
 from PyQt5.QtWidgets import (QApplication, QWidget, QLabel,
                                                        QLineEdit, QPushButton, QVBoxLayout)
 from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QIcon
 
 class WeatherApp(QWidget):
     def __init__(self):
@@ -21,6 +22,7 @@ class WeatherApp(QWidget):
 
     def initUI(self):
         self.setWindowTitle("Weather App")
+        self.setWindowIcon(QIcon('assests\\icon.jpg'))
 
         vbox = QVBoxLayout()
 
